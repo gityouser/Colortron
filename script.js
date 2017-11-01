853e161558abd334b6229d078e473a07c427a402
Serbanescu <eugen.serbanescu@hmhco.com>
2017-11-01 19:30:09 +0000

    refactor whatever

--- script.js
+++ script.js
@@ -1,48 +1,45 @@
 const squareSide = 100;
+const wrapper = document.querySelector('.wrapper');
 
 function boxMaker() {
   const perRow = Math.floor(document.body.offsetWidth / squareSide);
   const rows = Math.floor(document.body.offsetHeight / squareSide);
-  for (i=0; i < perRow * rows; i++) {
-    let wrapper = document.querySelector('.wrapper')
+  for (i = 0; i < perRow * rows; i++) {
     let createDiv = document.createElement('div');
     wrapper.appendChild(createDiv);
     createDiv.classList.add('box');
     createDiv.addEventListener('mouseover', change);
   }
-  let boxes = document.querySelectorAll('.box');
   resetMaker();
-};
+}
 boxMaker();
 
-function change(i) {
-  var r = Math.floor(Math.random() * 256);
-  var g = Math.floor(Math.random() * 256);
-  var b = Math.floor(Math.random() * 256);
-  var mix = `rgb(${r},${g},${b})`;
-  i.target.style.backgroundColor = mix;
-  console.log("Div no. " + i.target.innerHTML + ' ' + mix);
+function change(event) {
+  const r = Math.round(Math.random() * 255);
+  const g = Math.round(Math.random() * 255);
+  const b = Math.round(Math.random() * 255);
+  const mix = `rgb(${r},${g},${b})`;
+  event.target.style.backgroundColor = mix;
+  console.log('Div no. ' + event.target.innerHTML + ' ' + mix);
 }
-
-const boxes = document.querySelectorAll('.box');
 
 function resetMaker() {
   let boxes = document.querySelectorAll('.box');
-  let randomBox = boxes[Math.floor(Math.random() * boxes.length)];
+  let randomBox = boxes[Math.round(Math.random() * (boxes.length - 1))];
 
-// No need to set data-id!
+  // No need to set data-id!
   // randomBox.dataset.id = "specialBox";
 
   let resetButton = document.createElement('button');
   randomBox.appendChild(resetButton);
   resetButton.classList.add('reset');
-  resetButton.innerHTML = "RESET";
+  resetButton.innerHTML = 'RESET';
 
   resetButton.addEventListener('mouseover', resetStyle);
   resetButton.addEventListener('mouseleave', zoomOut);
   resetButton.addEventListener('click', springClean);
 
-//REMOVED as redundant!
+  //REMOVED as redundant!
   // for (i = 0; i < boxes.length; i++) {
   //   let boxesArray = Array.from(boxes);
   //
@@ -61,26 +58,26 @@ function resetMaker() {
 }
 
 function resetStyle() {
-  document.querySelector('.reset').style.cssText = "color: black; border: 2px outset lightgoldenrodyellow;  box-shadow: 2px 2px 40px 10px gold; transform: scale(1.1); transition: .4s"
+  document.querySelector('.reset').style.cssText =
+    'color: black; border: 2px outset lightgoldenrodyellow;  box-shadow: 2px 2px 40px 10px gold; transform: scale(1.1); transition: .4s';
 }
 
 function zoomOut() {
-  document.querySelector('.reset').style.transform ='scale(1)';
-  console.log("Zoom Out");
+  document.querySelector('.reset').style.transform = 'scale(1)';
+  console.log('Zoom Out');
 }
 
 function springClean() {
-  let wrapper = document.querySelector('.wrapper')
   while (wrapper.hasChildNodes()) {
     wrapper.removeChild(wrapper.firstChild);
-//REMOVED as redundant!
-  // for (i = 0; i < boxes.length; i++) {
-  //   let boxesArray = Array.from(boxes);
-  //   boxesArray[i].style.backgroundColor = "grey";
-  //     let wrapper = document.querySelector('.wrapper')
-  //     while (wrapper.hasChildNodes()) {
-  //       wrapper.removeChild(wrapper.firstChild);
-  //     }
-    }
-    boxMaker();
+    //REMOVED as redundant!
+    // for (i = 0; i < boxes.length; i++) {
+    //   let boxesArray = Array.from(boxes);
+    //   boxesArray[i].style.backgroundColor = "grey";
+    //     let wrapper = document.querySelector('.wrapper')
+    //     while (wrapper.hasChildNodes()) {
+    //       wrapper.removeChild(wrapper.firstChild);
+    //     }
+  }
+  boxMaker();
 }
