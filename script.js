c85fdf803c90e8cdbaf52d53a30fe8039c555e8e
Serbanescu <eugen.serbanescu@hmhco.com>
2017-11-01 11:46:34 +0000

    feat: resize

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ const squareSide = 100;
 const wrapper = document.querySelector('.wrapper');
 
 function init() {
-  const perRow = Math.round(document.body.offsetWidth / squareSide);
-  const rows = Math.round(document.body.offsetHeight / squareSide);
+  // HACK: shitty reset
+  wrapper.innerHTML = '';
+  const perRow = Math.floor(document.body.offsetWidth / squareSide);
+  const rows = Math.floor(document.body.offsetHeight / squareSide);
   for (i = 0; i < perRow * rows; i++) {
     let square = document.createElement('div');
     square.classList.add('box');
@@ -18,5 +20,7 @@ function changeColor(e) {
   var b = Math.floor(Math.random() * 256);
   e.target.style.backgroundColor = `rgb(${r},${g},${b})`;
 }
+
+window.onresize = init;
 
 init();
