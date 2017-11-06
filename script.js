a502818184dcfe19b302059ab4790ae3d5e0a09e
Andrei Constantinescu <andreiconstantinescumail@gmail.com>
2017-11-06 11:18:34 +0200

    Pre timer commit

--- script.js
+++ script.js
@@ -1,28 +1,45 @@
-const squareSide = 100;
+const squareSide = 100; //px
 const wrapper = document.querySelector('.wrapper');
+const overlay = document.querySelector('.overlay');
+const startButton = document.querySelector('.start-button');
+const retryButton = document.querySelector(".retry-button");
+var boxes;
+var completed = false;
+var timer = 0;
+var currentTime = Date.now();
+startButton.addEventListener('click', toggleOverlay);
+startButton.addEventListener('click', generateBoxes);
 
-function boxMaker() {
+// while (completed !== true) {
+//   timer += 1;
+// }
+function generateBoxes() {
   const perRow = Math.floor(document.body.offsetWidth / squareSide);
   const rows = Math.floor(document.body.offsetHeight / squareSide);
   for (i = 0; i < perRow * rows; i++) {
     let createDiv = document.createElement('div');
     wrapper.appendChild(createDiv);
     createDiv.classList.add('box');
-    createDiv.addEventListener('mouseover', change);
+    createDiv.addEventListener('mouseover', changeColor);
   }
-  resetMaker();
+  assignResetBtn();
+  boxes = document.querySelectorAll('.box');
 }
-
-function change(event) {
+function changeColor(event) {
   const r = Math.round(Math.random() * 255);
   const g = Math.round(Math.random() * 255);
   const b = Math.round(Math.random() * 255);
   const mix = `rgb(${r},${g},${b})`;
   event.target.style.backgroundColor = mix;
+  console.log(`Div no. ${event.target.innerHTML} ${mix}`);
   if(!event.target.dataset.id) {
     event.target.dataset.id = 'completed';
     console.log("DATA-ID ADDED!!!");
   };
+  checkForCompletion();
+}
+
+function checkForCompletion() {
   const boxes = document.querySelectorAll('.box');
   let boxeZ = Array.from(boxes);
   const boxID = boxeZ.every(function(box){
@@ -31,64 +48,46 @@ function change(event) {
   if(boxeZ.every(function(index){
     return index.dataset.id == 'completed';
   })) {
-    console.log("WOOOHOOO!!!");
+    completed = true;
     toggleCongrats();
+    removeAllBoxes();
   }
-  console.log(`Div no. ${event.target.innerHTML} ${mix}`);
 }
 
-function resetMaker() {
+function assignResetBtn() {
   let boxes = document.querySelectorAll('.box');
   let randomBox = boxes[Math.round(Math.random() * (boxes.length - 1))];
 
-  // let resetButton = document.createElement('button');
-  // randomBox.appendChild(resetButton);
-  // resetButton.classList.add('reset');
-  // resetButton.innerHTML = 'RESET';
-  //
-  // resetButton.addEventListener('mouseover', resetStyle);
-  // resetButton.addEventListener('mouseleave', zoomOut);
-  // resetButton.addEventListener('click', springClean);
-
-// I discarded the above 'shait' and I replaced with below 'better shait'!
   randomBox.classList.add('reset');
   randomBox.innerHTML = 'RESET';
+  const reset = document.querySelector('.reset');
 
-  randomBox.addEventListener('mouseover', resetStyle);
-  randomBox.addEventListener('mouseleave', zoomOut);
-  randomBox.addEventListener('click', springClean);
+  randomBox.addEventListener('mouseover', () => reset.style.cssText =
+    'color: black; box-shadow: 2px 2px 40px 10px gold; transform: scale(1.1); transition: .4s');
+  randomBox.addEventListener('mouseleave', () => reset.style.transform = 'scale(1)');
+  randomBox.addEventListener('click', removeAllBoxes);
+  randomBox.addEventListener('click', toggleOverlay)
 }
 
-function resetStyle() {
-  document.querySelector('.reset').style.cssText =
-    'font-family: Helvetica, Arial, sans-serif; color: black; box-shadow: 2px 2px 40px 10px gold; transform: scale(1.1); transition: .4s';
-}
-
-function zoomOut() {
-  document.querySelector('.reset').style.transform = 'scale(1)';
-  console.log('Zoom Out');
-}
-
-function springClean() {
-  toggleOverlay();
+function removeAllBoxes() {
   while (wrapper.hasChildNodes()) {
     wrapper.removeChild(wrapper.firstChild);
   }
 }
 
-const overlay = document.querySelector('.overlay');
-const button = document.querySelector('.button');
-button.addEventListener('click', toggleOverlay);
-
 function toggleOverlay() {
   overlay.classList.toggle('hiddenOverlay');
   overlay.style.transition = "all 1s";
-  boxMaker();
 }
 
-const congrats = document.querySelector('.congrats');
 function toggleCongrats() {
+  const congrats = document.querySelector('.congrats');
   congrats.classList.toggle('hiddenCongrats');
-  congrats.style.transition = "all 1s";
-  console.log("CONGRATTTTSS!!");
 }
+
+retryButton.addEventListener('click', function () {
+  toggleCongrats();
+  removeAllBoxes();
+  toggleOverlay();
+
+})
